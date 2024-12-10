f834ee5f0c1ceea192e743f972d5a51f21c5f826
Nomiz-Crown <simon.andersen2@elev.ga.lbs.se>
2024-12-10 10:32:38 +0100

    jag hatar barn

--- Assets/Grägas.cs
+++ Assets/Grägas.cs
@@ -1,18 +1,32 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Grägas : MonoBehaviour
 {
+    public GameObject Brick;
+    public float delay;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Brick.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Invoke("DeactivateBrick", delay);
+    }
+
+    private void DeactivateBrick()
+    {
+        Brick.SetActive(false);
     }
 }
